687a7670cd052bf2e24bb97587daf854823acf75
danielosorio0224 <dosorioquimbayo@gmail.com>
2024-06-14 16:11:14 -0500

    course

---
+++ learning/course/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { TodoComponent } from './pages/todo/todo.component';
+import { TODO_DATA } from '../assets/todo';
+import { NTodo } from './models/todo.model';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, TodoComponent, CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  todoData = TODO_DATA;
+
+  getTodoInfo(val: NTodo.TodoData) {
+    console.log(val);
+  }
+
+  orderData() {
+    this.todoData.sort((a, b) => a.priority - b.priority);
+  }
+}
